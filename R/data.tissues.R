757f4e150171d6d5e5a69a6728bc69fd3ddde11e
Kyeongwon Lee <kwlee1718@gmail.com>
2025-08-08 18:38:55 -0400

    Fix @format tags: remove backticks from data documentation

--- R/data.tissues.R
+++ R/data.tissues.R
@@ -3,7 +3,7 @@
 #' The tissues data of colon cancer dataset,
 #' which includes gene expression values from 22 colon tumor tissues and 40 non-tumor tissues.
 #' @name tissues
-#' @format `numeric`
+#' @format numeric
 #' @docType data
 #' @source http://genomics-pubs.princeton.edu/oncology/affydata/.
 #' @keywords data
